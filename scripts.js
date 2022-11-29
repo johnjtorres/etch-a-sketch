19f8bcf2bf79ba111a6a4d6a6736bed32e3c31dc
John Torres <johnnyjamestorres@outlook.com>
2022-11-29 12:43:04 -0800

    Create function to return a new cell

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const etchASketch = document.querySelector('.etch-a-sketch');
+
+function createCell() {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  return cell;
+}
