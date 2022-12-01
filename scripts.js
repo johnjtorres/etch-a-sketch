fa0273ce6b8409f72cee54a616505add9a653659
John Torres <johnnyjamestorres@outlook.com>
2022-12-01 12:57:05 -0800

    Add feature to increment black level on each pass

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@
 
 const MAX_CANVAS_SIZE = 100;
 const MIN_CANVAS_SIZE = 16;
+const MAX_OPACITY = 1;
+
 const canvas = document.querySelector('.etch-a-sketch-canvas');
 const canvasWidth = canvas.clientWidth;
 const gridSizeBanner = document.querySelector('.grid-size');
@@ -56,9 +58,64 @@ function createGrid() {
 
 function colorCellBackground(event) {
   if (event.buttons !== 1 && event.type !== 'click') return;
-  let color = 'black';
+  const cell = event.currentTarget;
+  let color = 'rgba(0, 0, 0, 1)';
   if (getCurrentMode() === 'rgb') color = getRandomRGB();
-  event.currentTarget.style.backgroundColor = color;
+  if (getCurrentMode() === 'increment') color = incrementCellOpacity(cell);
+  cell.style.backgroundColor = color;
+}
+
+function incrementCellOpacity(cell) {
+  let curColor = window.getComputedStyle(cell).backgroundColor;
+  curColor = colorValues(curColor);
+  const opacity = parseFloat(curColor[3]) || 0;
+  let newColor = 'rgba(0, 0, 0, 0.1)';
+  if (opacity > 0) {
+    newColor = `rgba(0, 0, 0, ${Math.min(opacity + 0.1, MAX_OPACITY)})`;
+  }
+  return newColor;
+}
+
+function colorValues(color) {
+  if (!color) return;
+  if (color.toLowerCase() === 'transparent') return [0, 0, 0, 0];
+  if (color[0] === '#') {
+    if (color.length < 7) {
+      // convert #RGB and #RGBA to #RRGGBB and #RRGGBBAA
+      color =
+        '#' +
+        color[1] +
+        color[1] +
+        color[2] +
+        color[2] +
+        color[3] +
+        color[3] +
+        (color.length > 4 ? color[4] + color[4] : '');
+    }
+    return [
+      parseInt(color.substr(1, 2), 16),
+      parseInt(color.substr(3, 2), 16),
+      parseInt(color.substr(5, 2), 16),
+      color.length > 7 ? parseInt(color.substr(7, 2), 16) / 255 : 1,
+    ];
+  }
+  if (color.indexOf('rgb') === -1) {
+    // convert named colors
+    var temp_elem = document.body.appendChild(document.createElement('fictum')); // intentionally use unknown tag to lower chances of css rule override with !important
+    var flag = 'rgb(1, 2, 3)'; // this flag tested on chrome 59, ff 53, ie9, ie10, ie11, edge 14
+    temp_elem.style.color = flag;
+    if (temp_elem.style.color !== flag) return; // color set failed - some monstrous css rule is probably taking over the color of our object
+    temp_elem.style.color = color;
+    if (temp_elem.style.color === flag || temp_elem.style.color === '') return; // color parse failed
+    color = getComputedStyle(temp_elem).color;
+    document.body.removeChild(temp_elem);
+  }
+  if (color.indexOf('rgb') === 0) {
+    if (color.indexOf('rgba') === -1) color += ',1'; // convert 'rgb(R,G,B)' to 'rgb(R,G,B)A' which looks awful but will pass the regxep below
+    return color.match(/[\.\d]+/g).map(function (a) {
+      return +a;
+    });
+  }
 }
 
 function clearGrid() {
